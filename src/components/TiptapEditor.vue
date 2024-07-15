27560ef46b208b49b4fa1caf11d7fb33acce7d40
lanseria <zhangchao564265135@hotmail.com>
2024-07-15 08:58:08 +0800

    支持了黑暗模式

--- src/components/TiptapEditor.vue
+++ src/components/TiptapEditor.vue
@@ -90,7 +90,7 @@ onMounted(() => {
     ],
     editorProps: {
       attributes: {
-        class: 'text-sm prose max-w-full prose-truegray focus:outline-none',
+        class: 'text-sm prose dark:prose-invert max-w-full prose-truegray focus:outline-none',
         // class: 'prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
       },
     },
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,45 +1,45 @@
 <script setup lang="ts">
 const content = ref(`
-        <h2>
-          Hi there,
-        </h2>
-        <p>
-          this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-        </p>
-        <ul>
-          <li>
-            That’s a bullet list with one …
-          </li>
-          <li>
-            … or two list items.
-          </li>
-        </ul>
-        <p>
-          I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-        </p>
-        <blockquote>
-          Wow, that’s amazing. Good work, boy! 👏
-          <br />
-          — Mom
-        </blockquote>
+        <h2> Hi there, </h2> <p> this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists: </p> <ul> <li> That’s a bullet list with one … </li> <li> … or two list items. </li> </ul> <p> I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too. </p> <blockquote> Wow, that’s amazing. Good work, boy! 👏 <br /> — Mom </blockquote>
+        <h2> 嗨， </h2> <p> 这是一个关于<strong>Tiptap</strong>的基本示例。当然，它具备您在文本编辑器中预期的各种基本文本样式。但等到您看到列表时，就会惊叹不已： </p> <ul> <li> 这是一个带有一个……的项目符号列表。 </li> <li> ……或者两个列表项。 </li> </ul> <p> 我知道，我知道，这太令人印象深刻了。但这只是冰山一角。尝试一下，点击一下吧。别忘了查看其他示例。 </p> <blockquote> 哇，太棒了。好样的，孩子！👏 <br /> — 妈妈 </blockquote>
       `)
+
+function handleOpen(url: string) {
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col items-center">
+    <div class="mb-6 inline-flex cursor-default text-2xl font-300">
+      <div class="flex flex-col children:mx-auto" @click="handleOpen('https://tiptap.dev')">
+        <img class="inline-block h-18 w-18" src="/favicon.png">
+        <span class="mt--2 text-gray5">Tiptap</span>
+      </div>
+      <div text="3xl gray4" m="x-4 y-auto" i-carbon-add="" transform="" transition-all-500="" hover:rotate-135="" />
+      <div class="flex flex-col children:mx-auto" @click="handleOpen('https://arco.design')">
+        <img class="inline-block h-18 w-18" src="/arco.design.png">
+        <span class="mt--2 text-blue5">Arco.Design</span>
+      </div>
+      <div text="3xl gray4" m="x-4 y-auto" i-carbon-add="" transform="" transition-all-500="" hover:rotate-135="" />
+      <div class="flex flex-col children:mx-auto" @click="handleOpen('https://vuejs.org')">
+        <img class="inline-block h-18 w-18" src="/vue.js.png">
+        <span class="mt--2 text-green5">Vue.js</span>
+      </div>
+    </div>
     <TiptapEditor v-model="content" />
     <nav mt-6 inline-flex gap-2 text-xl>
       <button icon-btn @click="toggleDark()">
         <div i-carbon-sun dark:i-carbon-moon />
       </button>
-      <!--
+
       <a
         i-carbon-logo-github icon-btn
         rel="noreferrer"
-        href="https://github.com/lanseria/arco-uno-basic-template"
+        href="https://github.com/lanseria/arco-tiptap"
         target="_blank"
         title="GitHub"
-      /> -->
+      />
     </nav>
   </div>
 </template>
